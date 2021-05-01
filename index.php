f967021efe55f6efd5709bd6511b67a37d5073b6
camilo381999 <camilo-leguizamon1@upc.edu.co>
2021-04-30 22:06:33 -0500

    commit

--- index.php
+++ index.php
@@ -8,88 +8,107 @@ include_once('templates/iniciar-html.php');
 include_once('templates/menu.php');
 ?>
 
-<div class="container">
+<aside>
+    <table id="lista-carrito" class="table">
+        <thead>
+            <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</aside>
 
-    <h2 class="h2-titulos">Nuestros productos</h2>
-    <br>
 
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/iphone.png" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">iPhone 11 Pro Max</h4>
-                    <h5 class="card-title">$4.999.900</h5>
-                    <p class="card-text">Triple camara trasera, procesador A13 Bionic, memoria interna de 256/512 GB y batería de 3.900 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
+<div class="contenido">
+    <div class="container-fluid">
+        <br><br><br>
+        <h2 class="h2-titulos">Nuestros productos</h2>
+        <br>
+
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/iphone.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">iPhone 11 Pro Max</h4>
+                        <h5 class="card-title">$4.999.900</h5>
+                        <p class="card-text">Triple camara trasera, procesador A13 Bionic, memoria interna de 256/512 GB y batería de 3.900 mAh.</p>
+                        <a href="#" data-id="1" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/s21plus.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Samsung Galaxy S21+</h4>
+                        <h5 class="card-title">$5.149.900</h5>
+                        <p class="card-text">Triple camara trasera, procesador Exynos 2100, memoria interna de 128/256 GB y batería de 4.800 mAh.</p>
+                        <a href="#" data-id="2" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/note20-ultra.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Galaxy Note 20 Ultra</h4>
+                        <h5 class="card-title">$5.499.900</h5>
+                        <p class="card-text">Cuatro camaras traseras, procesador Exynos 990, memoria interna de 256/512 GB y batería de 4.500 mAh.</p>
+                        <a href="#" data-id="3" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/s21plus.png" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Samsung Galaxy S21+</h4>
-                    <h5 class="card-title">$5.149.900</h5>
-                    <p class="card-text">Triple camara trasera, procesador Exynos 2100, memoria interna de 128/256 GB y batería de 4.800 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
+        <br>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/a51.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Samsung Galaxy A51</h4>
+                        <h5 class="card-title">$1.199.900</h5>
+                        <p class="card-text">Cuatro camaras traseras, procesador Exynos 9611, memoria interna de 128 GB y batería de 4.000 mAh.</p>
+                        <a href="#" data-id="4" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/note20-ultra.png" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Galaxy Note 20 Ultra</h4>
-                    <h5 class="card-title">$5.499.900</h5>
-                    <p class="card-text">Cuatro camaras traseras, procesador Exynos 990, memoria interna de 256/512 GB y batería de 4.500 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/redmi-note9.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Xiaomi Redmi Note 9</h4>
+                        <h5 class="card-title">$999.900</h5>
+                        <p class="card-text">Cuatro camaras traseras, procesador MediaTek Helio G85, memoria interna de 128 Gb y batería de 5.020 mAh.</p>
+                        <a href="#" data-id="5" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/nokia.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Nokia 3.4</h4>
+                        <h5 class="card-title">$549.900</h5>
+                        <p class="card-text">Triple camara trasera, procesador Qualcomm Snapdragon 460, memoria interna de 64 GB y batería de 4.000 mAh.</p>
+                        <a href="#" data-id="6" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <br>
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/a51.png" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Samsung Galaxy A51</h4>
-                    <h5 class="card-title">$1.199.900</h5>
-                    <p class="card-text">Cuatro camaras traseras, procesador Exynos 9611, memoria interna de 128 GB y batería de 4.000 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/redmi-note9.png" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Xiaomi Redmi Note 9</h4>
-                    <h5 class="card-title">$999.900</h5>
-                    <p class="card-text">Cuatro camaras traseras, procesador MediaTek Helio G85, memoria interna de 128 Gb y batería de 5.020 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/nokia.jpg" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Nokia 3.4</h4>
-                    <h5 class="card-title">$549.900</h5>
-                    <p class="card-text">Triple camara trasera, procesador Qualcomm Snapdragon 460, memoria interna de 64 GB y batería de 4.000 mAh.</p>
-                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
 </div>
+
+
+
 
 
 <?php
